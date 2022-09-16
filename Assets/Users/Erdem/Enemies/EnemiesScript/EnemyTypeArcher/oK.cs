7f9d08246ee080bebe9f899244731bee129de018
erdemyildiz35 <90454576+erdemyildiz35@users.noreply.github.com>
2022-09-16 04:34:09 +0300

    OyunaKalbimiBıraktım

---
+++ Assets/Users/Erdem/Enemies/EnemiesScript/EnemyTypeArcher/oK.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oK : MonoBehaviour
+{
+   [SerializeField] float OkSpeed = 3f;
+    Rigidbody2D Rbok;
+
+    void Awake()
+    {
+        Rbok = GetComponent<Rigidbody2D>();
+      
+    }
+
+    public void Fırla(float DEger)
+    {
+        if (DEger > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+
+        }
+
+        Rbok.velocity = new Vector3(DEger, 0, 0);
+        Destroy(this.gameObject, 6f);
+
+    }
+
+    // Update is called once per frame
+   
+}
