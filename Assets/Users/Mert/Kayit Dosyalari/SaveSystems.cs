925d5f3eb7c3412a8004a591b4585c1dc7c1f5f1
MertO03 <mert.onur03@gmail.com>
2022-09-17 20:49:27 +0300

    Data

---
+++ Assets/Users/Mert/Kayit Dosyalari/SaveSystems.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveSystems
+{
+    public static void SavePlayer(PlayerMovementDeneme player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/player.zero";
+
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerDatas data = new PlayerDatas();
+    }
+}
