bb9708208942b90c2507996a6e60b4b9adfd74c4
MertO03 <mert.onur03@gmail.com>
2022-09-18 04:59:53 +0300

    GameOver

---
+++ Assets/Users/Mert/Scripts/RestartButtonController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButtonController : MonoBehaviour
+{
+    [SerializeField] MenuController menuCont;
+    [SerializeField] int Button›ndex;
+    [SerializeField] Animator anim;
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        if (menuCont.buttonIndex == Button›ndex && Input.GetAxis("Submit") == 1)
+        {
+            yield return new WaitForSeconds(0.6f);
+            SceneManager.LoadScene("GecisSahnesi");
+        }
+    }
+}
